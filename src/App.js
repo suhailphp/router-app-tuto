18e002ababfd5d34c1522c184ac95fbb2c4de9b1
suhailphp <suhailphp@gmail.com>
2020-07-03 21:02:18 +0400

    Basic navigation Finished with rect-router-dom , its have a problem

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import ProductDetails from "./components/productDetails";
 import NotFound from "./components/notFound";
+import { Route } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
@@ -13,6 +14,13 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <div className="content">
+          <Route path="/products" component={Products} />
+          <Route path="/products/:id" component={ProductDetails} />
+          <Route path="/posts" component={Posts} />
+          <Route path="/" component={Home} />
+          <Route path="/admin" component={Dashboard} />
+        </div>
       </div>
     );
   }
