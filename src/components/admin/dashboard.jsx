2ee9a3eacd42b80d3d3786f07e5168334271f3ce
suhailphp <suhailphp@gmail.com>
2020-07-10 15:47:32 +0400

    Nested Routing : Move admin side bar to a new component

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,22 +1,14 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Users from "./users";
 import Posts from "./posts";
+import SideBar from "./sideBar";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
-
-      <ul>
-        <li>
-          <Link to="/admin/users">User</Link>
-        </li>
-        <li>
-          <Link to="/admin/posts">Posts</Link>
-        </li>
-      </ul>
-
+      <SideBar />
       <Route path="/admin/users" component={Users} />
       <Route path="/admin/posts" component={Posts} />
     </div>
