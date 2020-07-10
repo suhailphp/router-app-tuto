b00ed541d11c4b974a40d6efddcbc6d86dc1d374
suhailphp <suhailphp@gmail.com>
2020-07-10 15:45:09 +0400

    Nested Routing : Creating new Sidebar in admin dashboard and calling Route in same page

--- src/components/admin/dashboard.jsx
+++ src/components/admin/dashboard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Route, Link } from "react-router-dom";
+import Users from "./users";
+import Posts from "./posts";
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+
+      <ul>
+        <li>
+          <Link to="/admin/users">User</Link>
+        </li>
+        <li>
+          <Link to="/admin/posts">Posts</Link>
+        </li>
+      </ul>
+
+      <Route path="/admin/users" component={Users} />
+      <Route path="/admin/posts" component={Posts} />
     </div>
   );
 };
